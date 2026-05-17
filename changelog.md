48cf6e8de5d292ecc71a6bf64197c106a4dd2fb8
IsotonicBunk <115648600+IsotonicBunk@users.noreply.github.com>
2026-05-17 23:23:19 +0300

    v1.0.1 (see changelog.md)

--- changelog.md
+++ changelog.md
@@ -1,2 +1,6 @@
+# 1.0.1
+- Fixed mod config.
+- Updating song info earlier.
+- Updated to Geode v5.7.1
 # 1.0.0
-- Initial release
+- Initial release.
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,12 @@ using namespace geode::prelude;
 #include <Geode/modify/SongInfoLayer.hpp>
 
 class $modify(MySongInfoLayer, SongInfoLayer) {
+    
+    bool init(gd::string songName, gd::string artistName, gd::string songURL, gd::string newgroundsURL, gd::string youtubeURL, gd::string facebookURL, int songID, gd::string songTags, int nongType) {
+        if (!SongInfoLayer::init(songName, artistName, songURL, newgroundsURL, youtubeURL, facebookURL, songID, songTags, nongType)) return false;
+        MusicDownloadManager::sharedState()->getSongInfo(songID, false);
+        return true;
+    }
     void onDownload(CCObject* sender) {
         if (!Mod::get()->getSettingValue<bool>("enabled")) return SongInfoLayer::onDownload(sender); // <- if disabled
         if (!m_songID) return SongInfoLayer::onDownload(sender); // <- if not a custom song
@@ -18,7 +24,7 @@ class $modify(MySongInfoLayer, SongInfoLayer) {
             MusicDownloadManager::sharedState()->getSongInfo(m_songID, false);
             log::warn("Failed to get song URL. Please try again!");
         } else log::debug("song link is {}", song->m_songUrl); 
-
+        
         web::openLinkInBrowser(url);
     }
 };
